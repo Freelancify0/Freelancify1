e5eac553927de84a957453300233c64847709190
Freelancify0 <freelancify0@gmail.com>
2025-05-02 22:53:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ checkGitHubConfig();
 export { checkGitHubConfig };
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://freelancify-pzti.onrender.com/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   
@@ -105,7 +105,7 @@ app.use((req, res, next) => {
 });
 // app.use("/api/github", githubOauthRoute);
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://freelancify-pzti.onrender.com/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"]
