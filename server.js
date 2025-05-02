314ecca5bc1880ee1771e26b0f9479f1c606d58b
Freelancify0 <freelancify0@gmail.com>
2025-05-02 23:08:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ checkGitHubConfig();
 export { checkGitHubConfig };
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", "https://freelancify-pzti.onrender.com/");
+  res.header("Access-Control-Allow-Origin", "https://freelancify-swart.vercel.app/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   
